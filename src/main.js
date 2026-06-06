0085dce1292e0a464cf7a3ae9e5ec7f05750e836
Martin <jhonholtan@gmail.com>
2026-06-06 16:22:31 +0300

    changes to main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 module.exports.loop = function() {
-    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1', {memory: {role: 'harvester'}});
+    //Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1', {memory: {role: 'harvester'}});
 }
